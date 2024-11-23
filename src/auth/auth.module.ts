12144ab682641bd2cdad194ef48ec04643e56d35
zouhair ridaoui <z.ridaoui94@outlook.com>
2024-11-23 19:32:37 +0300

    feat(auth): add auth strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      validationSchema: joi.object({
+        JWT_SECRET: joi.string().required(),
+        JWT_EXPIRATION: joi.string().required(),
+      }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}d`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
