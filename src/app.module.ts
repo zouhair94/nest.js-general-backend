b04794cd6d8e9c0e799838434f173af339ad4370
zouhair ridaoui <z.ridaoui94@outlook.com>
2024-11-23 19:11:13 +0300

    feat(auth): add auth module
     generated the initial auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './libs/core';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+
+@Module({
+  imports: [UserModule],
+  controllers: [],
+  providers: [AuthService],
+})
+export class AuthModule {}
