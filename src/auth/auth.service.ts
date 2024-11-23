a991d51f6242d6fdab1ee3eac021968ddb1e5615
zouhair ridaoui <z.ridaoui94@outlook.com>
2024-11-23 19:43:38 +0300

    feat(auth): implement login and guards

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
-
 import { UserService } from '../user/user.service';
 
 @Injectable()
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -13,5 +13,6 @@ import { UserRepository } from './user.repository';
   ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
