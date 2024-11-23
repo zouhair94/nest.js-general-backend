bdb3f5ed883f7e86d3f130c99dc3574356700774
zouhair ridaoui <z.ridaoui94@outlook.com>
2024-11-23 18:28:35 +0300

    feat(user): add the user tests

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,102 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { describe } from 'node:test';
+import { UserRepository } from './user.repository';
+import { UnauthorizedException } from '@nestjs/common';
 
-describe('UserService', () => {
-  let service: UserService;
+const usersRepositoryMock = () => ({
+  find: jest.fn(),
+  create: jest.fn(),
+  findOne: jest.fn(),
+});
+
+const usersMock = [
+  {
+    _id: 1,
+    username: 'test1',
+    name: 'test',
+    surname: 'testSurname',
+    email: 'test@test.com',
+    password: 'password',
+    role: 'admin',
+  },
+  {
+    _id: 2,
+    username: 'test2',
+    name: 'test',
+    surname: 'testSurname2',
+    email: 'test2@test.com',
+    password: 'password',
+    role: 'user',
+  },
+  {
+    _id: 3,
+    username: 'test3',
+    name: 'test',
+    surname: 'testSurname3',
+    email: 'test3@test.com',
+    password: 'password',
+    role: 'user',
+  },
+];
+
+const user = {
+  username: 'test4',
+  name: 'test',
+  surname: 'testSurname',
+  email: 'test4@test.com',
+  password: 'password',
+  role: 'user',
+};
+
+describe('UsersService', () => {
+  let userService: UserService;
+  let userRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+
+        {
+          provide: UserRepository,
+          useFactory: usersRepositoryMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('should test get users', () => {
+    it('get users returns a value', async () => {
+      userRepository?.find?.mockResolvedValue(usersMock);
+      const result = await userService.findAll();
+      expect(result).toEqual(usersMock);
+    });
+
+    it('should filter user', async () => {
+      // mock User when id = 0
+      const mockUser = usersMock[0];
+      userRepository?.find?.mockResolvedValue(mockUser);
+      const result = await userService.findAll({ _id: 1 });
+      expect(result).toEqual(mockUser);
+    });
+  });
+  describe('should test create user', () => {
+    it('should create new user', async () => {
+      userRepository?.findOne?.mockResolvedValue(null);
+      userRepository?.create?.mockResolvedValue(user);
+      const result = await userService.create(user);
+      expect(result).toEqual(user);
+    });
+
+    it('should raise an error if the user exists', async () => {
+      userRepository?.findOne?.mockResolvedValue(user);
+      await expect(userService.create(user)).rejects.toThrow(
+        UnauthorizedException,
+      );
+    });
   });
 });
